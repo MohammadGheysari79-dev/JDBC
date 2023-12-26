5cc353477649f5069db766d1e804be7d097064a1
MohammadGheysari <mohammad7gehysari@gmail.com>
2023-12-26 19:09:00 +0330

    Add login

--- src/main/java/training/Main.java
+++ src/main/java/training/Main.java
@@ -1,12 +1,45 @@
 package training;
 
-
-import training.dao.Dao;
 import training.daoImp.EmployeeDaoImp;
 import training.model.Employee;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Dao<Employee> dao = EmployeeDaoImp.getInstance();
+        System.out.println("Welcome");
+        mainMenu();
+        login();
+    }
+
+    private static void mainMenu() {
+
+    }
+
+    private static void login() {
+        int id = readInt("Enter your ID: ", "Wrong input!!!", true);
+        int password = readInt("Enter your password: ", "Wrong input!!!", true);
+        Employee employee = EmployeeDaoImp.getInstance().get(id);
+        if (employee == null || employee.getPassword() == password) {
+            System.out.println("Your ID or password is wrong.");
+        } else {
+            System.out.println("Welcome " + employee.getFirstName() + " " + employee.getLastName());
+            //todo : login is done. show dashboard.
+        }
+    }
+
+    private static int readInt(String msg, String errorMessage, boolean repeatOnError) {
+        do {
+            System.out.println(msg);
+            try {
+                return input.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println(errorMessage);
+            }
+        } while (repeatOnError);
+        throw new InputMismatchException();
     }
 }
